64c29c9d30d8c42f6530cbf2560c46582e420263
Anshumann Bele <158322083+AnshumanNB@users.noreply.github.com>
2025-09-27 16:58:05 +0530

    Update productRoutes.js

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,10 +1,10 @@
 const {getProduct, addProduct, updateProduct, deleteProduct} = require("../controller/productController")
-
+const auth = require("../Middleware/auth")
 const route = require("express").Router()
 
 route.get("/", getProduct)
-route.post("/", addProduct)
-route.put("/:id", updateProduct)
-route.delete("/:id", deleteProduct)
+route.post("/", auth, addProduct)
+route.put("/:id", auth, updateProduct)
+route.delete("/:id", auth, deleteProduct)
 
 module.exports = route
