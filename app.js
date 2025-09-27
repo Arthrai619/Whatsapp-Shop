6c1bb5bafe10f65e0d366d55eb865dbcd21c87a8
Arthrai619 <167521859+Arthrai619@users.noreply.github.com>
2025-09-28 02:28:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const userRoutes = require("./Routes/userRoutes")
 const productRoutes = require("./Routes/productRoutes")
 const categoryRoutes = require("./Routes/categoryRoutes")
 require("dotenv/config")
+const cloudinary = require('cloudinary').v2;
+
 
 const cors = require("cors")
 
@@ -19,6 +21,17 @@ app.get("/",(req,res)=>{
 app.use("/api/product",productRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/category",categoryRoutes)
+
+try {
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+    });
+    console.log("Cloudinary connected successfully!");
+} catch (error) {
+    console.log("Cloudinary connection error:", error);
+}
 
 app.listen(process.env.PORT)
 
