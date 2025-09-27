932f9baab594948b9f3fc79d6003bd6197210354
Anshumann Bele <158322083+AnshumanNB@users.noreply.github.com>
2025-09-27 16:57:43 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,11 @@
-const {getUser, addUser, updateUser, deleteUser} = require("../controller/userController")
+const {getUser, addUser, updateUser, deleteUser, loginUser} = require("../controller/userController")
 
 const route = require("express").Router()
 
 route.get("/", getUser)
 route.post("/", addUser)
-route.post("/login", updateUser)
-route.post("/login", deleteUser)
+route.post("/login", addUser)
+route.put("/:id", updateUser)
+route.delete("/:id", deleteUser)
 
 module.exports = route
