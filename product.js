ba2ba04a73e7b28f9faef46427a35f4b87d5e5f2
Anshumann Bele <158322083+AnshumanNB@users.noreply.github.com>
2025-09-27 15:35:20 +0530

    product added

---
+++ product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: True
+    },
+    description:{
+        type: String
+    },
+    price: {
+        type: Number,
+        required: True
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'category',
+        required: True
+    },
+    shopkeeper: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: True
+    },
+    img: {
+        type: String,
+    }
+})
+
+module.exports = mongoose.model("product", productSchema)
